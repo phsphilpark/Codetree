b392db591a4ad082d5e18fb1864c530584471abf
phsphilpark <phsphilpark@gmail.com>
2026-03-07 15:06:38 +0900

    Title: 윤년인가, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260307/윤년인가/is-leap-year.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if(n%4==0){
+            if((n%100==0)&&(n%400!=0)){
+                System.out.print("false");
+            }else{
+                System.out.print("true");
+            }
+        }else{
+            System.out.print("false");
+        }
+    }
+}
