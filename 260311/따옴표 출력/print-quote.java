72ac56dbdc62d88bfe7466b4baa2e15c4d964ee8
phsphilpark <phsphilpark@gmail.com>
2026-03-11 21:38:59 +0900

    Title: 따옴표 출력, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260311/따옴표 출력/print-quote.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        String text = "He says \"It's a really simple sentence\".";
+
+        System.out.print(text);
+    }
+}
