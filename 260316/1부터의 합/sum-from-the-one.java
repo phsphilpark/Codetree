dc0c4e54255c9440c3dd6b4a6467a80bb921907e
phsphilpark <phsphilpark@gmail.com>
2026-03-16 22:28:46 +0900

    Title: 1부터의 합, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260316/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int total=0;
+        for(int i=1;i!=0;i++){
+            total+=i;
+            if(total>=n){
+                System.out.print(i);
+                break;
+            }
+        }
+    }
+}
