b4d977408c2cea2215a8fed2ea2cbf673aa885e2
phsphilpark <phsphilpark@gmail.com>
2026-03-21 23:04:08 +0900

    Title: 짝수의 합 n번 구하기, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260321/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            int total=0;
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            
+            for(int j = a; j<=b; j++){
+                if(j%2==0){
+                    total+=j;
+                }
+            }
+            System.out.println(total);
+        }
+    }
+}
