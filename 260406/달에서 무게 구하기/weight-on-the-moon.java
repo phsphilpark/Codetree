8f646bae225f9d395ba2a2199220cf4bb1a5b94f
phsphilpark <phsphilpark@gmail.com>
2026-04-06 23:17:09 +0900

    Title: 달에서 무게 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260406/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int a=13;
+        double g = 0.165;
+
+        System.out.printf("%d * %.6f = %f",a,g,a*g);
+    }
+}
