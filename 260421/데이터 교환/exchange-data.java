3a1c68d852429654e65cbedfb0b265aab61e8e83
phsphilpark <phsphilpark@gmail.com>
2026-04-21 21:49:52 +0900

    Title: 데이터 교환, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260421/데이터 교환/exchange-data.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=5,b=6,c=7;
+        int tmp;
+        tmp=c;
+        c=b;
+        b=a;
+        a=tmp;
+
+        System.out.printf("%d\n%d\n%d",a,b,c);
+
+    }
+}
