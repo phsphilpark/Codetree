25f487c32870cea00ec47e94af2895e86bbc2d2b
phsphilpark <phsphilpark@gmail.com>
2026-04-29 21:56:22 +0900

    Title: 합을 복사하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=1,b=2,c=3;
+        a=a+b+c;
+        b=a;
+        c=a;
+        System.out.printf("%d %d %d",a,b,c);
+    }
+}
