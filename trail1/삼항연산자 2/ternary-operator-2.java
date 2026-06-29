17cbb5117b92d47b827ebc1fa996c41d2ecef265
phsphilpark <phsphilpark@gmail.com>
2026-06-29 10:21:27 +0900

    [Passed] 삼항연산자 2 (137ms, 10MB)

---
+++ trail1/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if(a==1){
+            System.out.print("t");
+        }else{
+            System.out.print("f");
+        }
+    }
+}
