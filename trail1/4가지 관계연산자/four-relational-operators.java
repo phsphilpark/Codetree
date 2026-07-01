df55241b487bffbc7ad2bf4f0d46cef23fba8737
phsphilpark <phsphilpark@gmail.com>
2026-07-01 09:23:25 +0900

    [Passed] 4가지 관계연산자 (135ms, 10MB)

---
+++ trail1/4가지 관계연산자/four-relational-operators.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a>=b){
+            System.out.print("1\n");
+        }else{
+            System.out.print("0\n");
+        }
+        if(a>b){
+            System.out.print("1\n");
+        }else{
+            System.out.print("0\n");
+        }
+        if(b>=a){
+            System.out.print("1\n");
+        }else{
+            System.out.print("0\n");
+        }
+        if(b>a){
+            System.out.print("1\n");
+        }else{
+            System.out.print("0\n");
+        }
+    }
+}
