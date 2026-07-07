3b0738723fef3e8b8d797ade8dceccb1a4be6b5d
phsphilpark <phsphilpark@gmail.com>
2026-07-07 15:39:03 +0900

    [Passed] 입력받는 수 부터 100까지 출력 (140ms, 10MB)

---
+++ trail1/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        for(int i = a; i<=100; i++){
+            System.out.printf("%d ",i);
+        }
+    }
+}
