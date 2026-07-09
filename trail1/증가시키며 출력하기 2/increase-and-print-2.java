44271b6a783fe86ba0b505f36c8a437be8300224
phsphilpark <phsphilpark@gmail.com>
2026-07-09 14:47:48 +0900

    [Passed] 증가시키며 출력하기 2 (99ms, 9MB)

---
+++ trail1/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int i = 10;
+        while(true){
+            System.out.printf("%d ",i);
+            i++;
+            if(i>=27){
+                break;
+            }
+        }
+    }
+}
