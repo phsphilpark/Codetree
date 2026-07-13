4ae0bb534f3c1690264dc06c71baf11758f06d12
phsphilpark <phsphilpark@gmail.com>
2026-07-13 21:58:48 +0900

    [Passed] N번 더하기 (130ms, 11MB)

---
+++ trail1/N번 더하기/add-n-times.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+
+        int i = 0;
+        while(i<n){
+            System.out.printf("%d\n",a+n);
+            a+=n;
+            i++;
+        }
+    }
+}
