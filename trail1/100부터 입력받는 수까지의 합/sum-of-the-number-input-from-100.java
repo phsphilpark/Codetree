e95761c970daba999b0053e59b25f615cef07df4
phsphilpark <phsphilpark@gmail.com>
2026-07-22 08:48:52 +0900

    [Passed] 100부터 입력받는 수까지의 합 (133ms, 10MB)

---
+++ trail1/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int total = 0;
+        for(int i = n; i <= 100; i++){
+            total+=i;
+        }
+        System.out.printf("%d",total);
+    }
+}
