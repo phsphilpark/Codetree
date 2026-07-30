93620236264dc0f007dd4dee3109e465413cb516
phsphilpark <phsphilpark@gmail.com>
2026-07-30 23:34:28 +0900

    [Passed] 친근하지 않은 수 (130ms, 10MB)

---
+++ trail1/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int total = 0;
+
+    for(int i = 1; i <= n; i++){
+        if(i%2==0||i%3==0||i%5==0){
+            continue;
+        }else total++;
+    }
+    System.out.printf("%d",total);
+
+    }
+}
